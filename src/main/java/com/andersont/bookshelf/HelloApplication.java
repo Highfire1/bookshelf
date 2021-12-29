8aa1b6f01d8d64e52e710eafb66a7971e6a3b92b
Highfire1 <tseng.andersonn@gmail.com>
2021-12-28 16:38:19 -0800

    updates

--- src/main/java/com/andersont/bookshelf/HelloApplication.java
+++ src/main/java/com/andersont/bookshelf/HelloApplication.java
@@ -4,15 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // initialize language bundles
+        InputStream stream = new FileInputStream("resources/bundles/bundle.properties");
+        Properties prop = System.getProperties();
+        prop.load(stream);
+
+        // create gui window
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
+        stage.setTitle(prop.getProperty("windowTitle"));
+        //stage.getIcons().add(new Image("resources/assets/book.png"));
+        stage.initStyle(StageStyle.DECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/andersont/bookshelf/HelloController.java
+++ src/main/java/com/andersont/bookshelf/HelloController.java
@@ -1,14 +1,38 @@
 package com.andersont.bookshelf;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    public MenuItem createBook;
+
+    @FXML
+    public MenuItem createShelf;
+
+    @FXML
+    public MenuItem deleteShelf;
+
+    @FXML
+    public CheckMenuItem darkTheme;
+
+    @FXML
+    public MenuItem importData;
+
+    @FXML
+    public MenuItem exportData;
+
+    @FXML
+    public Menu autofill;
+
+    @FXML
+    public MenuItem about;
+
+    @FXML
+    public GridPane gridpane;
 }
